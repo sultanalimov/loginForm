4b2964a1394fed106568aa0075708170f9632f0d
Sultan Alimov <sultanalimov@mail.com>
2020-07-31 15:28:35 +0500

    project upload

---
+++ src/pages/Sing_up/Sign_up.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './css/Sign_up.module.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+export default function Sign_up() {
+    return (
+        <div className={classes.Sign_up}>
+            <Navbar />
+            <h1>Sign up Page</h1>
+        </div>
+    )
+}
